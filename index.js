24a53a2d2da23714fedb2e71fe25135a2c9b839a
Foram Thakker <foramthakker.120@gmail.com>
2025-12-23 11:57:21 +0530

    Ready for deployment

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ app.get("/", (req, res) => {
   res.send("RecipeFinder Backend is running");
 });
 
-const PORT = process.env.PORT || 5000; // use Render's PORT or fallback to 5000 for local
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Backend running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
+
 
 mongoose
   .connect(process.env.MONGO_URI)
